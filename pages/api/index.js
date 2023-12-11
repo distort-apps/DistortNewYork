f42146c57b1d57de8bf6c098d78a4db99ba1237a
Daniel <danoladesign@gmail.com>
2023-12-11 03:55:30 -0500

    Implememnted api handler for featured shows

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,5 +1,34 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import fs from 'fs'
+import path from 'path'
+import {
+  connectDatabase,
+  getAllDocuments,
+} from '../../helpers/db-util'
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+async function handler (req, res) {
+  let client
+  try {
+    client = await connectDatabase()
+  } catch (error) {
+    res.status(500).json({ message: 'Connecting to db failed🚬💀💀💀' })
+    return
+  }
+  // GET ALL shows
+  if (req.method === 'GET') {
+    let documents
+    try {
+      documents = await getAllDocuments(
+        client,
+        'shows',
+        { _id: -1 },
+        { isFeatured: true }
+      )
+      res.status(200).json({ shows: documents })
+    } catch (error) {
+      res.status(500).json({ message: 'Error fetching documentsdocuments🚬🚬' })
+    }
+  }
+  client.close()
 }
+
+export default handler
