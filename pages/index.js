a033a2ae380b78dbe444fb7728ccd5190123bbbb
Daniel <danoladesign@gmail.com>
2023-12-11 03:59:58 -0500

    feature events are now fetched from MongoDB

--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,15 @@ function HomePage(props) {
   )
 }
 
-export default HomePage
-
 export async function getStaticProps(){
 
-  const featuredShows = getFeaturedShows()
+  // const featuredShows = await getFeaturedShows()
+
+  const featuredShows = await fetch('http://localhost:3000/api')
+  .then(res => res.json()).then(data => {
+      let shows = data.shows
+    return shows
+  })
 
   return {
     props: {
@@ -20,3 +24,5 @@ export async function getStaticProps(){
     revalidate: 60
   }
 }
+
+export default HomePage
