28194c8f9977d370888220ca35948f157916045b
Daniel <danoladesign@gmail.com>
2023-12-18 18:56:26 -0500

    update contact page for deployment

--- components/input/contact-body.js
+++ components/input/contact-body.js
@@ -1,13 +1,14 @@
 import classes from './contact-body.module.css'
+import MailToButton from '../ui/mail-to-button'
 function ContactBodyPage () {
   return (
     <div className={classes.contact}>
       <h2>Write DistortNewYork @:</h2>
-      <p>distortnewyork@gmail.com</p>
-      <h3>or send a flyer to:</h3>
+      <MailToButton label="DistortNewYork@gmail.com" mailto="mailto:distortnewyork@gmail.com" />
+      {/* <h3>or send a flyer to:</h3>
       <p>407 Himrod</p>
       <p>Apt 2R</p>
-      <p>BRKLYN NY, 11237</p>
+      <p>BRKLYN NY, 11237</p> */}
       <hr />
     </div>
   )
