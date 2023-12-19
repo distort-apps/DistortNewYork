7ee0780ef68e2a7bdeb9040f86b31404c6523d7d
Daniel <danoladesign@gmail.com>
2023-12-18 19:10:01 -0500

    updated contact page

--- components/input/contact-body.js
+++ components/input/contact-body.js
@@ -4,14 +4,13 @@ function ContactBodyPage () {
   return (
     <div className={classes.contact}>
       <h2>Write DistortNewYork @:</h2>
-      <MailToButton label="DistortNewYork@gmail.com" mailto="mailto:distortnewyork@gmail.com" />
-      {/* <h3>or send a flyer to:</h3>
-      <p>407 Himrod</p>
-      <p>Apt 2R</p>
-      <p>BRKLYN NY, 11237</p> */}
+      <MailToButton
+        label='DistortNewYork@gmail.com'
+        mailto='mailto:distortnewyork@gmail.com'
+      />
       <hr />
     </div>
   )
 }
-export default ContactBodyPage
 
+export default ContactBodyPage
