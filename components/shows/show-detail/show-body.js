4e20aeb06d33b1b6d5fcdaba771436dd1aea79e0
Daniel <danoladesign@gmail.com>
2023-12-21 17:38:13 -0500

    fixed date issue

--- components/shows/show-detail/show-body.js
+++ components/shows/show-detail/show-body.js
@@ -1,11 +1,12 @@
 import classes from './show-body.module.css'
 
 function ShowBody ({ title, image, excerpt, date, price, location, time }) {
-  const readableDate = new Date(date).toLocaleDateString('en-Us', {
+  const readableDate = new Intl.DateTimeFormat('en-US', {
     day: 'numeric',
     month: 'long',
-    year: 'numeric'
-  })
+    year: 'numeric',
+    timeZone: 'UTC' // Set time zone to UTC
+  }).format(new Date(date));
   return (
     <div className={classes.body}>
       <div>
