4f614b7a7844082c4aeadac0df45e52262704fea
Daniel <danoladesign@gmail.com>
2023-12-22 01:50:11 -0500

    move analytics component to head

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,18 @@
 import Head from 'next/head'
 import Layout from '@/components/layout/layout'
 import '@/styles/globals.css'
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from '@vercel/analytics/react'
 
 export default function App ({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
         <title>DistortNewYork</title>
-        <meta name="description" content="Shows in NYC and all boroughs"/>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name='description' content='Shows in NYC and all boroughs' />
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        <Analytics />
       </Head>
-      <Component {...pageProps}> 
-      <Analytics />
-      </Component> 
+      <Component {...pageProps} />
     </Layout>
   )
 }
