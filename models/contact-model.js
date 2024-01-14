5e16151473e94372070d2ff2e3a3ec13a274d458
Daniel <danoladesign@gmail.com>
2024-01-13 22:13:41 -0500

    DSNY023: Updated uri connection string | troubleshooting deployment after refactor

--- models/contact-model.js
+++ models/contact-model.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const cantactSchema = new mongoose.Schema({
     email: {
         type: String,
-        required: true
+        required: false
     },
     title: {
         type: String,
