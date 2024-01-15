a3feb0fa7daa85c020592f1b78954bccda290124
Daniel <danoladesign@gmail.com>
2024-01-15 17:20:53 -0500

    DSNY028: Updating db connection function to troubleshoot serverless functions errors in production | check connection status - if noconnection check for cached promise - create new connection promise - wait for the connection promise to resolve

--- helpers/db-util.js
+++ helpers/db-util.js
@@ -1,6 +1,28 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
 
 export async function connectDb() {
-  const uri = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.${process.env.mongodb_database}/gagz?retryWrites=true&w=majority`
-  return mongoose.connect(uri);
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    const uri = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.${process.env.mongodb_database}/gagz?retryWrites=true&w=majority`
+    cached.promise = mongoose.connect(uri, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+
+  // Wait for the connection promise to resolve
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
