6e69e9ce585c700b7ecd20f9be8d98d2a3d550bc
Daniel <danoladesign@gmail.com>
2024-01-16 16:16:16 -0500

    DSNY034: Troubleshooting deployment

--- components/shows/show-detail/show-header.js
+++ components/shows/show-detail/show-header.js
@@ -3,10 +3,10 @@ import classes from './show-header.module.css'
 function ShowHeader ({ title }) {
   return (
     <div className={classes.headerWrapper}>
-    <header className={classes.header}>
-      <h1>{title}</h1>
-    </header>
-    <hr/>
+      <header className={classes.header}>
+        <h1>{title}</h1>
+      </header>
+      <hr />
     </div>
   )
 }
