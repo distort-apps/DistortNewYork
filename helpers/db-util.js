40ba6424ae0ecb49fa40815404309d23fcaee838
Daniel <danoladesign@gmail.com>
2024-01-16 18:28:08 -0500

    DSNY037: Redeploy

--- helpers/db-util.js
+++ helpers/db-util.js
@@ -17,7 +17,7 @@ export async function connectDb() {
     };
 
     const uri = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.${process.env.mongodb_database}/gagz?retryWrites=true&w=majority`
-    cached.promise = mongoose.connect(uri, opts).then((mongoose) => {
+    cached.promise = await mongoose.connect(uri, opts).then((mongoose) => {
       return mongoose;
     });
   }
