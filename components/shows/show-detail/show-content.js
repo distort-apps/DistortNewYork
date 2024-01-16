c3e666171e04d5b9d24b555324297a23220efc7d
Daniel <danoladesign@gmail.com>
2024-01-16 17:24:40 -0500

    DSNY036: FIxing deployment

--- components/shows/show-detail/show-content.js
+++ components/shows/show-detail/show-content.js
@@ -4,21 +4,20 @@ import ShowFooter from './show-footer'
 import classes from './show-content.module.css'
 
 function ShowContent ({ show }) {
-
   return (
-      <article className={classes.content}>
-        <ShowHeader title={show.title} />
-        <ShowBody
-          title={show.title}
-          image={show.image}
-          date={show.date}
-          location={show.location} 
-          time={show.time}
-          price={show.price}
-          genre={show.genre}
-        />
-        <ShowFooter excerpt={show.excerpt}/>
-      </article>
+    <article className={classes.content}>
+      <ShowHeader title={show.title} />
+      <ShowBody
+        title={show.title}
+        image={show.image}
+        date={show.date}
+        location={show.location}
+        time={show.time}
+        price={show.price}
+        genre={show.genre}
+      />
+      <ShowFooter excerpt={show.excerpt} />
+    </article>
   )
 }
 export default ShowContent
