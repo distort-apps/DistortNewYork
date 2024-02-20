181c92003404885d717e40af9d9ec140a80b4668
Daniel <danoladesign@gmail.com>
2024-02-20 12:55:01 -0500

    DSNY055: fixed mistake in logo

--- components/layout/logo.js
+++ components/layout/logo.js
@@ -1,14 +1,14 @@
-import classes from './logo.module.css';
-import { usePathname } from 'next/navigation';
-function Logo() {
-  const pathname = usePathname();
+import classes from './logo.module.css'
+import { usePathname } from 'next/navigation'
+function Logo () {
+  const pathname = usePathname()
   return (
     <section className={classes.logo}>
-      <div className={pathname === '/' ? classes.active : undefined}>Distort
-        NewYork
+      <div className={pathname === '/' ? classes.active : undefined}>
+        DistortNewYork
       </div>
     </section>
-  );
+  )
 }
 
-export default Logo;
+export default Logo
