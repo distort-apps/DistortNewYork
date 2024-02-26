193287d871c35d486e7c23c3a52615a04b91afa1
Daniel <danoladesign@gmail.com>
2024-02-26 02:13:19 -0500

    DSNY056: Updated query for fetchFeaturedShows and fetchAllShows to only retrieve shows with the current days date and later

--- helpers/api-util.js
+++ helpers/api-util.js
@@ -4,7 +4,13 @@ export async function fetchFeaturedShows () {
   try {
     const connection = await connectDb()
 
-    const featuredShows = await Show.find({ isFeatured: true })
+    const today = new Date()
+    today.setUTCHours(0, 0, 0, 0)
+
+    const featuredShows = await Show.find({
+      isFeatured: true,
+      date: { $gte: today }
+    })
       .sort({ date: 1 })
       .exec()
 
@@ -18,7 +24,17 @@ export async function fetchFeaturedShows () {
 export async function fetchAllShows () {
   try {
     const connection = await connectDb()
-    const shows = await Show.find({}).sort({ date: 1 }).limit(500).exec()
+
+    const today = new Date()
+    today.setUTCHours(0, 0, 0, 0)
+
+    const shows = await Show.find({
+      date: { $gte: today }
+    })
+      .sort({ date: 1 })
+      .limit(500)
+      .exec()
+
     return shows
   } catch (error) {
     console.error('Error in getAllShows:', error)
@@ -26,17 +42,13 @@ export async function fetchAllShows () {
   }
 }
 
-export async function getShowById(id) {
+export async function getShowById (id) {
   try {
     const connection = await connectDb()
-    const show = await Show.findById(id);
-    return show;
+    const show = await Show.findById(id)
+    return show
   } catch (error) {
-    console.error('Error in getShowById:', error);
-    throw new Error('Internal Server Error');
+    console.error('Error in getShowById:', error)
+    throw new Error('Internal Server Error')
   }
 }
-
-
-
-
