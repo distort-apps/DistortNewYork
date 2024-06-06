d0030202c17d7066088c8d93e79f09dbd9a03bac
Daniel <danoladesign@gmail.com>
2024-06-06 19:09:44 -0400

    DSNY080: Added The Altanta Solidarity Fund to info on Submit page

--- components/input/contact-body.js
+++ components/input/contact-body.js
@@ -5,37 +5,63 @@ function ContactBodyPage () {
   return (
     <div className={classes.contact}>
       <div>
-      <h2>Write DistortNewYork @:</h2>
-      <MailToButton
-        label='contact@distortnewyork.com'
-        mailto='mailto:contact@distortnewyork.com'
-      />
-      <hr />
-    </div>
-    <div>
-      <h2>Mayday Space</h2>
-      <p>A HOME FOR MOVEMENTS, SOCIAL JUSTICE ACTIVISM, AND COMMUNITY EVENTS</p>
-      <Link
-        href='https://maydayspace.org/'
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        https://maydayspace.org/
-      </Link>
-      <hr />
-    </div>
-    <div>
-      <h2>Save Project Reach</h2>
-      <p>Raising funds to help empower young people and marginalized communities.</p>
-      <Link
-        href='https://www.instagram.com/saveprojectreach/'
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        @saveprojectreach
-      </Link>
-      <hr />
-    </div>
+        <h2>Write DistortNewYork @:</h2>
+        <MailToButton
+          label='contact@distortnewyork.com'
+          mailto='mailto:contact@distortnewyork.com'
+        />
+        <hr />
+      </div>
+      <div>
+        <h2>Mayday Space</h2>
+        <p>
+          A HOME FOR MOVEMENTS, SOCIAL JUSTICE ACTIVISM, AND COMMUNITY EVENTS
+        </p>
+        <Link
+          href='https://maydayspace.org/'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          https://maydayspace.org/
+        </Link>
+        <hr />
+      </div>
+      <div>
+        <h2>Save Project Reach</h2>
+        <p>
+          Raising funds to help empower young people and marginalized
+          communities.
+        </p>
+        <Link
+          href='https://www.instagram.com/saveprojectreach/'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          @saveprojectreach
+        </Link>
+        <hr />
+      </div>
+      <div>
+        <h2>The Atlanta Solidarity Fund</h2>
+        <p>
+          The Atlanta Solidarity Fund bails out activists who are arrested for
+          participating in social justice movements, and helps them get access
+          to lawyers.
+        </p>
+        <p>
+          Your contribution will go directly to supporting those facing
+          repression. Please contribute what you can.
+        </p>
+        <p>When we stand together, we are strong!</p>
+        <Link
+          href='https://actionnetwork.org/fundraising/contribute-to-the-atlanta-solidarity-fund'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          https://actionnetwork.org/fundraising/contribute-to-the-atlanta-solidarity-fund'
+        </Link>
+        <hr />
+      </div>
     </div>
   )
 }
