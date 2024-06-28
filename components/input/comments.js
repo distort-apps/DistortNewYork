e77c01dfc83b471068958ee6d3b13e0f1a79d9ab
Daniel <danoladesign@gmail.com>
2024-06-28 14:39:05 -0400

    DSNY082: Adding auto scroll on comment handler.

--- components/input/comments.js
+++ components/input/comments.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 
 import CommentList from './comment-list'
 import NewComment from './new-comment'
@@ -9,6 +9,7 @@ function Comments (props) {
 
   const [showComments, setShowComments] = useState(false)
   const [comments, setComments] = useState([])
+  const commentSectionRef = useRef(null)
 
   useEffect(() => {
     if(showComments) {
@@ -18,6 +19,12 @@ function Comments (props) {
       })
     }
   }, [showComments])
+
+  useEffect(() => {
+    if (showComments && commentSectionRef.current) {
+      commentSectionRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [comments]);
 
   function toggleCommentsHandler () {
     setShowComments(prevStatus => !prevStatus)
@@ -37,7 +44,7 @@ function Comments (props) {
 
 
   return (
-    <section className={classes.comments}>
+    <section className={classes.comments} ref={commentSectionRef}>
       <button onClick={toggleCommentsHandler}>
         {showComments ? 'hide' : 'show'} comments
       </button>
