25e298d7e55ee2c3f66fab7c9a1fae0d94f50d27
Daniel <danoladesign@gmail.com>
2024-07-17 22:22:22 -0400

    DSNY100: Added logo from Paige <3

--- components/layout/logo.js
+++ components/layout/logo.js
@@ -1,11 +1,14 @@
 import classes from './logo.module.css'
 import { usePathname } from 'next/navigation'
+import logo from '../../public/images/logo.PNG'
+import Image from 'next/image'
+
 function Logo () {
   const pathname = usePathname()
   return (
     <section className={classes.logo}>
       <div className={pathname === '/' ? classes.active : undefined}>
-        DistortNewYork
+      <Image src={logo} alt="Logo" width={100} height={100} />
       </div>
     </section>
   )
